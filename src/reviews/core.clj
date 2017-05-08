503eca0f51772dc9d0cf782097231a99190a98de
puolimatkankrouvi <cde_1414@yahoo.com>
2017-05-08 16:54:01 +0300

    Added form

--- src/reviews/core.clj
+++ src/reviews/core.clj
@@ -1,6 +1,7 @@
 (ns reviews.core
   (:use compojure.core)
   (:use hiccup.core)
+  (:use hiccup.form)
   (:require
     [ring.adapter.jetty :as jetty]
     [compojure.route :as route]))
@@ -12,15 +13,17 @@
 (defn fahr [] (
   str ( + (* celcius 1.8 ) 32 ) ))
 
-(defn index [] (
-  html [:form [ [:input{ :type "text" :value celcius}]  "°C"  [:input {:type "text" :value (fahr)} "°F" ] ] ] ) )
+(defn input [] (
+  html [:form {:action "/" :method "post"} [ :input{ :type "text" :value celcius}]  "°C"  [:input {:type "text" :value (fahr)} ] "°F" [:input {:type "button" :value "Calculate"}  ] ]  ) )
 
 
 
 (defroutes main-router
-  (GET "/" {param :params} (index) ) 
+  (GET "/" {param :params} (input) ) 
+  (POST "/" [:as request] (str (request :multipart-params)) )
   (route/resources "/") 
-  (route/not-found "404 not found") )
+  (route/not-found "404 not found")
+  )
 
 (defn -main []
   (jetty/run-jetty
