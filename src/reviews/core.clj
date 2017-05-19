39e1c765b63e81daed20ef07df39498bc5a195ea
puolimatkankrouvi <cde_1414@yahoo.com>
2017-05-19 11:53:13 +0300

    fixing form

--- src/reviews/core.clj
+++ src/reviews/core.clj
@@ -3,6 +3,7 @@
   (:use hiccup.core)
   (:use hiccup.form)
   (:use hiccup.page)
+  (:use ring.middleware.params)
   (:require
     [ring.adapter.jetty :as jetty]
     [compojure.route :as route]
@@ -12,11 +13,13 @@
 (def celcius 20)
 
 
+
+
 (defn fahr [value] (
   str ( + (* value 1.8 ) 32 ) ))
 
 (defn input [fahrenheit] (
-  html [:head [:title  "Celcius to Fahrenheit converter" ] (include-css "style.css" ) ] [:body [:form {:action "/result/:celcius" :method "get"} [ :input{ :type "text" :value celcius}]  "°C"  [:input {:type "submit" :value "Calculate"} ] ] ]  ) )
+  html [:head [:title  "Celcius to Fahrenheit converter" ] (include-css "public/tyyli.css" ) ] [:body [:form {:action "/result/" :method "get"} [ :input{ :type "text" :value celcius}]  "°C"  [:input {:type "submit" :value "Calculate"} ] ] ]  ) )
 
 (defn result [celcius]
   (html [:p (fahr celcius)] "°F" ))
@@ -29,14 +32,17 @@
 (defroutes main-router
   (GET "/" {param :params} (input [] ) ) 
   ;Celcius null
-  (GET "/result/:celcius" [celcius]
-        (result (parse-int celcius) )
+  (GET "/result/" [params]
+        (result (parse-int (get params "celcius")))
   )
   (route/resources "/") 
   (route/not-found "404 not found")
   )
 
+(def app
+  (wrap-params main-router))
+
 (defn -main []
   (jetty/run-jetty
-    main-router
+    app
     {:port 5000}))
